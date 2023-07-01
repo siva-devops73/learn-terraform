94e0bc209b65affdb64d6551455336a2cc942205
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-01 09:21:58 +0530

    learn-terraform

---
+++ 02-outputs/output.tf
@@ -0,0 +1,12 @@
+output "sample" {
+  value = "Hello Siva Kumar"
+}
+
+# Any string in terraform need to be quoted with double quotes only.
+
+
+output "sample1" {
+  value = 1000
+}
+
+# Numbers and Booleans(true , false) no need double quotes
