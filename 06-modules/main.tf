3ebf79298d0f4ad42bf06bd5fc019f7d04c265d6
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-02 09:27:25 +0530

    learn-terraform

--- 06-modules/main.tf
+++ 06-modules/main.tf
@@ -8,3 +8,11 @@ module "sample2" {
   source = "./ec2"
   name = "sample2"
 }
+
+output "sample1" {
+  value = module.sample1.public_ip
+}
+
+output "sample2" {
+  value = module.sample2.public_ip
+}
