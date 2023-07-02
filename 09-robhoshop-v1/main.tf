fb913705de7250eb61c612e57d779fcf89f740f9
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-02 10:27:16 +0530

    learn-terraform

---
+++ 09-robhoshop-v1/main.tf
@@ -0,0 +1,54 @@
+module "frontend" {
+  source = "./ec2"
+  name = "frontend"
+}
+
+module "mongodb" {
+  source = "./ec2"
+  name = "mongodb"
+}
+
+module "catalogue" {
+  source = "./ec2"
+  name = "catalogue"
+}
+
+module "cart" {
+  source = "./ec2"
+  name = "cart"
+}
+
+module "user" {
+  source = "./ec2"
+  name = "user"
+}
+
+module "mysql" {
+  source = "./ec2"
+  name = "mysql"
+}
+
+module "shipping" {
+  source = "./ec2"
+  name = "shipping"
+}
+
+module "rabbitmq" {
+  source = "./ec2"
+  name = "rabbitmq"
+}
+
+module "payment" {
+  source = "./ec2"
+  name = "payment"
+}
+
+module "dispatch" {
+  source = "./ec2"
+  name = "dispatch"
+}
+
+module "redis" {
+  source = "./ec2"
+  name = "redis"
+}
