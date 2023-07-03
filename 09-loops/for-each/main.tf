7abba8cfde3ad465e7979793b46565709b51444e
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-03 14:18:55 +0530

    learn-terraform

---
+++ 09-loops/for-each/main.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "example" {
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
+
+resource "aws_instance" "web" {
+  for_each      = var.instances
+  ami           = data.aws_ami.example.id
+  instance_type = lookup(each.value, "instance_type", "t3.small")
+
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "instances" {
+  default = {
+
+    frontend = {
+      name = "frontend"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.nano"
+    }
+    mongodb = {
+      name = "mongodb"
+      instance_type = "t3.nano"
+    }
+  }
+}
