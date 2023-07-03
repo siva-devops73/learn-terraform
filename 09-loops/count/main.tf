c61826dad9c20953abd1134041bc9a43ea15321b
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-03 13:53:37 +0530

    learn-terraform

--- 09-loops/count/main.tf
+++ 09-loops/count/main.tf
@@ -1,16 +1,16 @@
 data "aws_ami" "example" {
-  count = 3
   owners = ["973714476881"]
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
 }
 
 resource "aws_instance" "web" {
+  count = length(var.instances)
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloSiva"
+    Name = "HelloSiva - ${count.index}"
   }
 }
 
